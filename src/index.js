67092d3f8548287aa950c9c0e1cfd8cb6a321075
irinagrishkevich <bonivomal1996@gmail.com>
2023-01-18 15:54:48 +0100

    matrix >>> sorted mas

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
 module.exports = function towelSort(matrix) {
   if (typeof matrix === 'undefined') {
     return []
+  } else {
+    const res = []
+    if (matrix.length > 0) {
+      let result
+      for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 === 1) {
+          matrix[i].reverse()
+        }
+        result = matrix.concat()
+      }
+      for (let i = 0; i < result.length; i++) {
+        for (let j = 0; j < result[i].length; j++) {
+          res.push(result[i][j])
+        }
+      }
+      return res
+    } else {
+      return []
+    }
   }
 }
